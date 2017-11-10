feb4887c58d16d56ab59f1b5dec357490d44b527
luca-venturi <luca.venturi.92@gmail.com>
2017-11-09 20:20:19 -0500

    fix as bef

--- hw2/sk_svm.py
+++ hw2/sk_svm.py
@@ -3,8 +3,7 @@
 import numpy as np
 import sklearn.preprocessing as prep
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score 
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 
 # write data from 'data/spam' into np.array
@@ -26,6 +25,12 @@ yTest = data[-nTest:,-1].astype(np.int)
 
 nFeat = xTrain.shape[1]
 
+# scale data
+
+scaler = prep.StandardScaler()
+xTrain = scaler.fit_transform(xTrain)
+xTest = scaler.transform(xTest)
+
 # run cross validation
 
 K = 7
@@ -36,7 +41,7 @@ nBatch = 10
 scores = {}
 for d in degree_range:
 	for C in C_range:
-		svm = make_pipeline(prep.StandardScaler(), SVC(C=C, kernel='poly', degree = d))
+		svm = SVC(C=C, kernel='poly', degree = d)
 		tmp = cross_val_score(svm,xTrain,yTrain,cv = nBatch)
 		scores[d,C,'mean'] = tmp.mean()
 		scores[d,C,'std'] = tmp.std()
@@ -47,6 +52,8 @@ for d in degree_range:
 colors = {1:'r',2:'b',3:'g',4:'k'}
 errors = np.array([[(1.-scores[d,C,'mean']) for C in C_range] for d in degree_range])
 std = np.array([[scores[d,C,'std'] for C in C_range] for d in degree_range])
+print(errors)
+best_index = np.unravel_index(np.argmin(errors), errors.shape)
 
 log2_C_range = np.log2(np.array(C_range))
 for i in range(len(degree_range)):
@@ -59,7 +66,6 @@ plt.show()
 
 # find best (d,C)
 
-best_index = numpy.unravel_index(errors.argmin(), errors.shape)
 best_d = degree_range[best_index[0]]
 best_C = C_range[best_index[1]]
 print(d,C)
@@ -68,7 +74,7 @@ print(d,C)
 
 scores = {}
 for d in degree_range:
-	svm = make_pipeline(prep.StandardScaler(), SVC(C=best_C, kernel='poly', degree = d))
+	svm = SVC(C=best_C, kernel='poly', degree = d)
 	tmp = cross_val_score(svm,xTrain,yTrain,cv = nBatch)
 	scores[d,'cv'] = tmp.mean()
 	print('d = ',d,' C = ',best_C,' -> ',scores[d,'cv'])
@@ -79,9 +85,6 @@ plt.plot(degree_range,errors_cv,'b')
 # plot test errors
 
 for d in degree_range:
-	scaler = prep.StandardScaler()
-	xTrain = scaler.fit_transfom(xTrain)
-	xTest = scaler.transform(xTest)
 	svm = SVC(C=best_C, kernel='poly', degree = d)
 	svm.fit(xTrain,yTrain)
 	scores[d,'n_support'] = sum(svm.n_support_)
@@ -100,6 +103,78 @@ plt.show()
 
 # how to evaluate number of support vectors on margin hyperplanes
 
-# 
+# customized kernel
 
+def my_kernel(x,y):
+	c = 0.1
+	n = 4
+	g = 0.
+	p = np.dot(x,y.T)
+	v = [1,1,2,2,2,1,1]
+	for i in range(n+3):
+		g += v[i] * (p ** (i+2))
+	return g
 
+# run cross validation
+
+K = 7
+degree_range = [1,2,3,4]
+C_range = [2**(i-K) for i in range(2*K +1)]
+nBatch = 10
+
+scores = {}
+for d in degree_range:
+	for C in C_range:
+		svm = SVC(C=C, kernel=my_kernel, degree = d)
+		tmp = cross_val_score(svm,xTrain,yTrain,cv = nBatch)
+		scores[d,C,'mean'] = tmp.mean()
+		scores[d,C,'std'] = tmp.std()
+		print('d = ',d,' C = ',C,' -> ',scores[d,C,'mean'])
+		
+# plot CV errors
+
+colors = {1:'r',2:'b',3:'g',4:'k'}
+errors = np.array([[(1.-scores[d,C,'mean']) for C in C_range] for d in degree_range])
+std = np.array([[scores[d,C,'std'] for C in C_range] for d in degree_range])
+print(errors)
+
+log2_C_range = np.log2(np.array(C_range))
+for i in range(len(degree_range)):
+	plt.plot(log2_C_range,errors[i,:],colors[degree_range[i]], label='d = ' + str(degree_range[i]))
+	#plt.plot(log2_C_range,errors[i,:] + std[i,:],colors[degree_range[i]]+'--')
+	#plt.plot(log2_C_range,errors[i,:] - std[i,:],colors[degree_range[i]]+'--')
+	#plt.errorbar(log2_C_range, errors[i,:], xerr = None, yerr=std[i,:], colors[degree_range[i]], label='d = ' + str(degree_range[i]))
+plt.legend()
+plt.show()
+
+# find best (d,C)
+
+best_index = np.unravel_index(np.argmin(errors), errors.shape)
+best_d = degree_range[best_index[0]]
+best_C = C_range[best_index[1]]
+print(d,C)
+
+# plot CV errors
+
+scores = {}
+for d in degree_range:
+	svm = SVC(C=best_C, kernel=my_kernel, degree = d)
+	tmp = cross_val_score(svm,xTrain,yTrain,cv = nBatch)
+	scores[d,'cv'] = tmp.mean()
+	print('d = ',d,' C = ',best_C,' -> ',scores[d,'cv'])
+	
+errors_cv = np.array([(1.-scores[d,'cv']) for d in degree_range])
+plt.plot(degree_range,errors_cv,'b')
+
+# plot test errors
+
+for d in degree_range:
+	svm = SVC(C=best_C, kernel=my_kernel, degree = d)
+	svm.fit(xTrain,yTrain)
+	scores[d,'n_support'] = sum(svm.n_support_)
+	scores[d,'test'] = svm.score(xTest,yTest)
+	print('d = ',d,' C = ',best_C,' -> ',scores[d,'test'])
+	
+errors_test = np.array([(1.-scores[d,'test']) for d in degree_range])
+plt.plot(degree_range,errors_test,'y')
+plt.show()
